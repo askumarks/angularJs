ad85030c3a09a6d6fad2729de0cbd1dce5ee604f
Rony J Samuel <ronysunrise@gmail.com>
2016-10-28 16:55:43 +0530

    Filtering done

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.config(function($routeProvider){
 		templateUrl: 'templates/validation.html'
 	
 	})
+	.when('/filter',{
+		templateUrl: 'templates/filter.html',
+		controller : 'filterCtrlr'
+	
+	})
 	.otherwise({
 		templateUrl : 'templates/home.html'
 	});
@@ -25,9 +30,10 @@ app.directive('quickLinks', function(){
 		templateUrl: "templates/quicklinks.html",
 		controller: function($scope){
 			$scope.links = [
-				{'label' : 'binding', 'url' : '#binding'},
-				{'label' : 'controllers', 'url' : '#contorllers'},
-				{'label' : 'validation', 'url': '#validation'}
+				{'label' : 'Binding', 'url' : '#binding'},
+				{'label' : 'Controllers', 'url' : '#contorllers'},
+				{'label' : 'Validation', 'url': '#validation'},
+				{'label' : 'Filter', 'url': '#filter'}
 			]
 		}
 	}
@@ -36,4 +42,27 @@ app.directive('quickLinks', function(){
 app.controller("bindCtrl", function($scope){
 	$scope.name = "Rony & AK";
 
+});
+app.controller("filterCtrlr", function($scope){
+	$scope.names=[
+		{fName:"Anantha",lName:"Krishnan"},
+		{fName:"Rony",lName:"Samuel"},
+		{fName:"Midhun",lName:"George"},
+		{fName:"Jobin",lName:"Mathew"},
+		{fName:"Nabeel",lName:"Thalakkat"},
+		{fName:"Antu",lName:"Kuriakose"},
+		{fName:"Keerthi",lName:"Nair"},
+		{fName:"Asita",lName:"Raju"},
+		{fName:"Anju",lName:"Antony"},
+
+	];
+	$scope.price = 0;
+
+	$scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
+    }
+    $scope.convert = function(y) {
+    	$scope.myNum = parseInt(y);
+    }
+ 
 });

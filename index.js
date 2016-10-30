b8619e2735455810b168a3926f86efb49542df42
aswanth <askumar@suyati.com>
2016-10-30 22:10:03 +0530

    advanced examples added

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.config(function($routeProvider){
 		templateUrl:'templates/form.html',
 		controller:"formcontrol"
 	})
+	.when('/broadcast-emit',{
+		templateUrl:'templates/broadcast.html',
+	})
+	.when('/api',{
+		templateUrl:'templates/api.html',
+		controller: 'apiController'
+	})
 	.otherwise({
 		templateUrl : 'templates/home.html'
 	});
@@ -32,12 +39,51 @@ app.directive('quickLinks', function(){
 				{'label' : 'bindings', 'url' : '#binding'},
 				{'label' : 'controllers', 'url' : '#contorllers'},
 				{'label' : 'filters', 'url' : '#filters'},
-				{'label':'Form','url':'#form'}
+				{'label':'Form','url':'#form'},
+				{'label':'Broadcast & Emit','url':'#broadcast-emit'},
+				{'label':'Rest Api','url':'#api'}
 			]
 		}
 	}
 });
 
+app.directive('parentDiv', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/parent-div.html',
+		controller: function($scope){
+			$scope.counter = 0;
+			$scope.msg = [];
+			$scope.$on('childClick', function(event, data){
+				$scope.counter +=1;
+				$scope.msg.push($scope.counter + '. ' + data);
+			})
+		}
+	}
+
+});
+
+app.directive('childDiv', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/child-div.html',
+		controller: function($scope){
+			$scope.bcounter = 0;
+			$scope.click = function(){
+				$scope.$emit('childClick', 'hi clicked');
+			}
+		}
+	}
+
+});
+
+app.controller("apiController", function($scope, $http){
+	$scope.posts = [];
+	$http.get('posts.json').success(function(response){
+		$scope.posts = response;
+	});
+
+});
 app.controller("bindCtrl", function($scope){
 	$scope.name = "Jobin Mathew";
 	$scope.profession = "Developer";
